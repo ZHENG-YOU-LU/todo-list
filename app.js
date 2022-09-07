c912378ca8697f7f549eb7f3822db29b58971600
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-09-07 16:24:07 +0800

    feat: add method-override

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ const exphbs = require('express-handlebars')
 const mongoose = require('mongoose') // 載入 mongoose
 const Todo = require('./models/todo') // 載入 Todo model
 const bodyParser = require('body-parser') // 引用 body-parser
-const app = express()
+const methodOverride = require('method-override') // 載入 method-override
+const app = express() 
 
 // set MONGODB_URI
 console.log(process.env.MONGODB_URI)
@@ -27,6 +28,9 @@ app.set('view engine', 'hbs')
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
+
+// 設定每一筆請求都會透過 methodOverride 進行前置處理
+app.use(methodOverride("_method"))
 
 app.get('/', (req, res) => {
 	Todo.find() // 取出 Todo model 裡的所有資料
@@ -63,7 +67,7 @@ app.get('/todos/:id/edit', (req, res) => {
 		.catch(error => console.log(error))
 })
 
-app.post('/todos/:id/edit', (req, res) => {
+app.put('/todos/:id', (req, res) => {
 	const id = req.params.id
 	const { name, isDone } = req.body
 	return Todo.findById(id) //查詢資料
@@ -76,7 +80,7 @@ app.post('/todos/:id/edit', (req, res) => {
 		.catch(error => console.log(error))
 })
 
-app.post('/todos/:id/delete', (req, res) => {
+app.delete('/todos/:id', (req, res) => {
 	const id = req.params.id
 	return Todo.findById(id)
 		.then(todo => todo.remove())
