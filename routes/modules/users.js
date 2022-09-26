c9c4b29598915774ee625c468a19a57803f48c4d
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-09-26 11:55:17 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,7 +1,13 @@
 // 引用 Express 與 Express 路由器
 const express = require('express')
 const router = express.Router()
+
 router.get('/login', (req, res) => {
 	res.render('login')
 })
+
+router.get('/register' ,(req ,res) =>{
+	res.render('register')
+})
+
 module.exports = router
