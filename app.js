e6cc2af24d467720027918758a4e0d127cfc65de
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-09-26 17:26:57 +0800

    feat: add express-session

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const session = require('express-session') //引用 express-session
 const exphbs = require('express-handlebars') //樣板引擎
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override') // 載入 method-override
@@ -12,6 +13,12 @@ const app = express()
 
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname:'.hbs'}))
 app.set('view engine', 'hbs')
+
+app.use(session({
+	secret: 'ThisIsMySecret',
+	resave: false,
+	saveUninitialized: true
+}))
 
 // 用 app.use 規定每一筆請求都需要透過 body-parser 進行前置處理
 app.use(bodyParser.urlencoded({ extended: true }))
