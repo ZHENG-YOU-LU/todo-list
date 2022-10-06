be15cf1bce3209e6725e56d51727d5f01188200c
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-10-06 15:01:08 +0800

    feat: add bcrypt.js

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -5,7 +5,8 @@ const router = express.Router()
 const User = require('../../models/user')
 // 引用 passport
 const passport = require('passport')
-
+// 引用 bcrypt 套件	
+const bcrypt = require('bcryptjs')
 router.get('/login', (req, res) => {
 	res.render('login')
 })
@@ -52,11 +53,14 @@ router.post('/register' ,(req, res) => {
 			})
 		}
 		// 如果還沒註冊：寫入資料庫
-		return User.create({
+		return bcrypt
+			.genSalt(10) // 產生「鹽」，並設定複雜度係數為 10
+			.then(salt => bcrypt.hash(password, salt)) // 為使用者密碼「加鹽」，產生雜湊值
+			.then(hash => User.create({
 				name,
 				email,
-				password
-			})
+				password: hash  // 用雜湊值取代原本的使用者密碼
+			}))
 			.then(() => res.redirect('/'))
 			.catch(err => console.log(err))
 	})
