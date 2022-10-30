0575ce359db06cec3ff1eb3c1ecf13205327689b
ZHENG-YOU-LU <yowyow222@gmail.com>
2022-10-30 22:29:13 +0800

    feat: add facebook login & dotenv

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const exphbs = require('express-handlebars') //樣板引擎
 const bodyParser = require('body-parser') // 引用 body-parser
 const methodOverride = require('method-override') // 載入 method-override
 const flash = require('connect-flash') // flash message
-const PORT = process.env.PORT || 3000
+
+if(process.env.NODE_ENV !== 'production') {
+	require('dotenv').config()
+}
 
 const routes = require('./routes') // 引用路由器
 require('./config/mongoose') //引用資料庫
@@ -13,12 +16,13 @@ console.log(process.env.MONGODB_URI)
 const usePassport = require('./config/passport') // 載入設定檔，要寫在 express-session 以後
 
 const app = express() 
+const PORT = process.env.PORT
 
 app.engine('hbs', exphbs({ defaultLayout: 'main' , extname:'.hbs'}))
 app.set('view engine', 'hbs')
 
 app.use(session({
-	secret: 'ThisIsMySecret',
+	secret: process.env.SESSION_SECRET,
 	resave: false,
 	saveUninitialized: true
 }))
--- config/mongoose.js
+++ config/mongoose.js
@@ -1,8 +1,8 @@
 const mongoose = require('mongoose') // 載入 mongoose
 
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }) // 設定連線到 mongoDB
+const MONGODB_URI = process.env.MONGODB_URI
 
-
+mongoose.connect(MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true, useCreateIndex: true }) // 設定連線到 mongoDB
 
 // 取得資料庫連線狀態
 const db = mongoose.connection
